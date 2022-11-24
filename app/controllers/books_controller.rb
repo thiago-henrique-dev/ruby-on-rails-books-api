32e7674b08a6dc9d3f34725f810e4809116d8ba5
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-24 15:08:24 -0300

    using json:api and active model serializers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books
   def index
     @books = Book.all
-    render json: @books
+    render json: @books,  include: [:authors], fields: { books: [:title, :num_pages], authors: [:full_name, :bar ]}
+
   end
 
   # GET /books/1
